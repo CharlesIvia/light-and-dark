c61db6ed66bd939d532c96df624c36d2b78eceae
Charles Ivia <iviacharles@gmail.com>
2020-09-12 18:21:14 +0300

    Local storage for dark-theme

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let body = document.querySelector("body");
 let toggleBtn = document.querySelector(".toggle-btn");
 let currentTheme = localStorage.getItem("currentTheme");
 
+if (currentTheme) {
+  body.classList.add("dark-theme");
+}
+
 toggleBtn.addEventListener("click", () => {
   body.classList.toggle("dark-theme");
+
+  if (body.classList.contains("dark-theme")) {
+    localStorage.setItem("currentTheme", "activeTheme");
+  } else {
+    localStorage.removeItem("currentTheme");
+  }
 });
